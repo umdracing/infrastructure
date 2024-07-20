5d03e9aa2a94ca18e6722f0e17c2cc60137e2179
Jonathan Irmscher <jonathan.irmscher@umdracing.de>
2024-07-20 16:22:51 +0200

    Modifications, return back to minimal example

--- flake.nix
+++ flake.nix
@@ -3,68 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-#    flake-utils.url = "github:numtide/flake-utils";
-    vps.url = "./VPS";
   };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nixos-shell
+        ];
 
-  outputs = { self, nixpkgs, flake-utils, vps, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        # VM configuration
-        vm = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            vps.nixosConfigurations.default.config
-            ({ config, ... }: {
-              virtualisation.vmVariant = {
-                virtualisation = {
-                  memorySize = 2048;
-                  cores = 2;
-                };
-              };
-            })
-          ];
-        };
-
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nix
-            colmena
-            qemu
-            nixos-shell
-          ];
-
-          shellHook = ''
-            echo "VPS development environment loaded"
-            echo "Use 'colmena build' to build the configuration"
-            echo "Use 'colmena apply' to deploy to the VM"
-            echo "Use 'nixos-shell --flake .#vm' to start the VM"
-          '';
-        };
-
-        # Colmena configuration
-        colmena = {
-          meta = {
-            description = "VPS deployment configuration";
-            nixpkgs = nixpkgs.outPath;
-            specialArgs = { inherit vps; };
-          };
-
-          vm = { name, nodes, ... }: {
-            deployment = {
-              targetHost = "localhost";
-              targetUser = "root";
-            };
-
-            imports = [ vps.nixosConfigurations.default.config ];
-          };
-        };
-
-        # VM configuration
-        nixosConfigurations.vm = vm;
-      });
+        shellHook = ''
+          echo "Welcome to the minimal dev environment!"
+        '';
+      };
+    };
 }
