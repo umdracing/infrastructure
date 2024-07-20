9fc3899552718402ee6400d96491a922b80bcf6e
Jonathan Irmscher <jonathan.irmscher@umdracing.de>
2024-07-20 16:41:55 +0200

    Added content to minimal VPS flake

---
+++ vps/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "NixOS configuration for VPS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ config, pkgs, ... }: {
+          boot.loader.systemd-boot.enable = true;
+          boot.loader.efi.canTouchEfiVariables = true;
+
+          users.users.alice = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+            initialPassword = "test";
+          };
+
+          environment.systemPackages = with pkgs; [
+            cowsay
+            lolcat
+          ];
+
+          system.stateVersion = "23.11";
+        })
+        # Add your custom modules here
+        # ./modules/example.nix
+        # ./system/configuration.nix
+      ];
+    };
+  };
+}
